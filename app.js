d88f1f1e65aef11c3d5c4a4ba166bb70f2fc53b1
Najeebat <isholanajeebatadedoyin@gmail.com>
2023-11-07 14:05:39 +0100

    quiz game

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
-const questions = [
+let questions = [
     {
-        question: "Which is the largest animal in the world",
+        question: "Who is the father of HTML?",
         answers: [
-            { text: "Shark", correct: false },
-            { text: "Blue whale", correct: true },
-            { text: "Elephant", correct: false },
-            { text: "Giraffe", correct: false },
+            { text: "Rasmus Lerdorf", correct: false },
+            { text: "Tim Berners-Lee", correct: true },
+            { text: "Brendan Eich", correct: false },
+            { text: "Sergey Brin", correct: false },
         ]
     },
 
     {
-        question: "Which is the smallest country in the world",
+        question: "HTML stands for __________ ?",
         answers: [
-            { text: "Shark", correct: false },
-            { text: "Blue whale", correct: true },
-            { text: "Elephant", correct: false },
-            { text: "Giraffe", correct: false },
+            { text: " HyperText Markup Language", correct: true },
+            { text: "HyperText Machine Language", correct: false },
+            { text: "HyperText Marking Language", correct: false },
+            { text: "HighText Marking Language", correct: false },
         ]
     },
 
     {
-        question: "Which is the largest animal in the world",
+        question: "In which part of the HTML metadata is contained?",
         answers: [
-            { text: "Shark", correct: false },
-            { text: "Blue whale", correct: true },
-            { text: "Elephant", correct: false },
-            { text: "Giraffe", correct: false },
+            { text: " head tag", correct: true },
+            { text: "title tag", correct: false },
+            { text: "html tag", correct: false },
+            { text: "body tag", correct: false},
         ]
     },
 
     {
-        question: "Which is the largest animal in the world",        answers: [
-            { text: "Shark", correct: false },
-            { text: "Blue whale", correct: true },
-            { text: "Elephant", correct: false },
-            { text: "Giraffe", correct: false },
+        question: " Which of the following is used to read an HTML page and render it?",       
+         answers: [
+            { text: "Web server", correct: false },
+            { text: "Web network", correct: false },
+            { text: "Web browser", correct: true },
+            { text: "Web matrix", correct: false },
         ]
     },
 ];
 
-const questionElement = document.getElementById("question");
-const answerButtonElement = document.getElementById("answerButtons");
-const nextButton = document.getElementById("nextButton");
-// const scoreBoard = document.getElementById("score-board");
+let questionElement = document.getElementById("question");
+let answerButtonElement = document.getElementById("answerButtons");
+let startButton = document.getElementById("startButton");
+let nextButton = document.getElementById("nextButton");
+let playAgainButton = document.getElementById("playAgainButton");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
+startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", nextQuiz);
+playAgainButton.addEventListener("click", startQuiz);
+
 nextButton.addEventListener("click", () => {
-    currentQuestionIndex++;
+    currentQuestionIndex;
     if (currentQuestionIndex < questions.length) {
         showQuestion();
     } else {
@@ -56,40 +62,47 @@ nextButton.addEventListener("click", () => {
     }
 });
 
+
+ 
+
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
+    startButton.style.display = "none";
+    playAgainButton.style.display ="none" //hide display button at start
+
+    answerButtonElement.style.display = "block"
 }
 
 function showQuestion() {
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNumber = currentQuestionIndex + 1;
+    let questionNumber = currentQuestionIndex +1;
     questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
+
+    while (answerButtonElement.firstChild) {
+        answerButtonElement.removeChild(answerButtonElement.firstChild);
+    }
 
     // this will clear the previous answer in the buttons and 
    // replace it with the current question and answer in app.js
-    answerButtonElement.innerHTML = "";
+    // answerButtonElement.innerHTML = "";
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtonElement.appendChild(button);
+
         if(answer.correct){
             button.dataset.correct = answer.correct;
         }
         button.addEventListener("click", selectAnswer);
 
-
-        button.addEventListener("click", () => {
-            if (answer.correct) {
-                score += 10;
-            }
-        });
-        // answerButtonElement.appendChild(button);
+        
     });
+    nextButton.style.display = "none";
 }
 function selectAnswer(e) {
     const selectedBtn = e.target;
@@ -97,7 +110,7 @@ function selectAnswer(e) {
 
     if (isCorrect) {
         selectedBtn.classList.add("correct");
-        score += 10;
+        score += 1; //increase score by one of the correct answer
     } else {
         selectedBtn.classList.add("incorrect");
     }
@@ -115,33 +128,28 @@ function selectAnswer(e) {
 
 
 function showScore() {
-    resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    const percentageScore = Math.round((score / questions.length) * 100);
+    questionElement.innerHTML = `You scored ${percentageScore}% !`;
+    nextButton.style.display = "none";
+    answerButtonElement.style.display = "none"
     nextButton.innerHTML = "Play Again";
-    nextButton.style.display = "block";
+    playAgainButton.style.display = "block"; //show the play again button
 }
 
 
 
-function handleNextButton() {
+
+function nextQuiz(){
     currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length ) {
+    if (currentQuestionIndex < questions.length){
         showQuestion();
     } else {
         showScore();
     }
+
 }
 
 
-
-
-nextButton.addEventListener("click", () => {
-    if (currentQuestionIndex < questions.length) {
-        handleNextButton();
-    } else {
-        startQuiz();
-    }
-});
-
 startQuiz();
+
 
